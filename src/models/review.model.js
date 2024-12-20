925237f61176474ddc8f49cc4010367f77dbb202
Rajnish Deshmukh <rajnishdeshmukh05@gmail.com>
2024-12-20 17:25:58 +0530

    add review, payment and courseProgress schema

---
+++ src/models/review.model.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const reviewSchema = new Schema(
+  {
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    course: {
+      type: Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Review = model("Review", reviewSchema);
